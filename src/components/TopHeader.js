779222f3f8679a5bf80bb275035028d0b4a306f0
quentin <ak47zy@gmail.com>
2018-11-23 17:52:09 +0800

    change the main page css

--- src/components/TopHeader.js
+++ src/components/TopHeader.js
@@ -1,20 +1,54 @@
-import React from 'react';
-import {Layout, Menu} from 'antd';
-const {Header} = Layout;
+import React,{Component} from 'react';
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+import { Layout, Menu } from 'antd';
+const { Header } = Layout;
 
 export default class TopHeader extends React.Component {
   render() {
     return (
       <Header className="header topheader">
-        <div className="logo"/>
-        <Menu
+        <div className="logo" />
+        {/* <Navbar inverse collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#brand">React-Bootstrap</a>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav>
+              <NavItem eventKey={1} href="#">
+                Link
+      </NavItem>
+              <NavItem eventKey={2} href="#">
+                Link
+      </NavItem>
+              <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+                <MenuItem eventKey={3.1}>Action</MenuItem>
+                <MenuItem eventKey={3.2}>Another action</MenuItem>
+                <MenuItem eventKey={3.3}>Something else here</MenuItem>
+                <MenuItem divider />
+                <MenuItem eventKey={3.3}>Separated link</MenuItem>
+              </NavDropdown>
+            </Nav>
+            <Nav pullRight>
+              <NavItem eventKey={1} href="#">
+                Link Right
+      </NavItem>
+              <NavItem eventKey={2} href="#">
+                Link Right
+      </NavItem>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar> */}
+        {/* <Menu
           theme="dark"
           mode="horizontal"
           defaultSelectedKeys={['2']}
-          style={{lineHeight: '64px', position: 'fixed', width: '100%', zIndex: 3}}
+          style={{lineHeight: '60px', position: 'fixed', width: '100%', zIndex: 3}}
         >
           <Menu.Item key="1">&nbsp;</Menu.Item>
-        </Menu>
+        </Menu> */}
       </Header>
     );
   }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -285,7 +285,6 @@ class RegistrationForm extends React.Component {
           <div className="row top_tiles">
             <div className="animated flipInY col-lg-3 col-md-3 col-sm-6 col-xs-12">
               <div className="tile-stats">
-                <div className="icon"><i className="fa fa-institution"></i></div>
                 <div className="count">{this.state.EnterpriseCount}</div>
                 <h3>注册企业数</h3>
                 <p>企业总数.</p>
@@ -293,7 +292,6 @@ class RegistrationForm extends React.Component {
             </div>
             <div className="animated flipInY col-lg-3 col-md-3 col-sm-6 col-xs-12">
               <div className="tile-stats">
-                <div className="icon"><i className="fa fa-area-chart"></i></div>
                 <div className="count">{this.state.PVCount}</div>
                 <h3>昨日PV数</h3>
                 <p>昨日PV数.</p>
@@ -301,7 +299,6 @@ class RegistrationForm extends React.Component {
             </div>
             <div className="animated flipInY col-lg-3 col-md-3 col-sm-6 col-xs-12">
               <div className="tile-stats">
-                <div className="icon"><i className="fa fa-exclamation"></i></div>
                 <div className="count">{this.state.ExceptionCount}</div>
                 <h3>昨日异常数</h3>
                 <p>昨日异常数.</p>
@@ -309,7 +306,6 @@ class RegistrationForm extends React.Component {
             </div>
             <div className="animated flipInY col-lg-3 col-md-3 col-sm-6 col-xs-12">
               <div className="tile-stats">
-                <div className="icon"><i className="fa fa-user"></i></div>
                 <div className="count">{this.state.ActiveUserCount}</div>
                 <h3>昨日活跃用户数</h3>
                 <p>昨日活跃用户数.</p>
@@ -325,8 +321,7 @@ class RegistrationForm extends React.Component {
                 <Axis name="CreateTime" />
                 <Axis name="ViewCount" visible={true} />
                 <Tooltip crosshairs={{ type: "y" }} />
-                <Geom type="area" position="CreateTime*ViewCount" />
-                <Geom type='point' position="CreateTime*ViewCount" shape={'circle'} />
+                <Geom type="line" position="CreateTime*ViewCount" />
               </Chart>
               </Col>
             </Row>
@@ -338,7 +333,6 @@ class RegistrationForm extends React.Component {
                   <Axis name="ExceptionCount" visible={true} />
                   <Tooltip crosshairs={{ type: "y" }} />
                   <Geom color={['#FF6347']} type="line" position="HappenTime*ExceptionCount" />
-                  <Geom color={['#FF6347']} type='point' position="HappenTime*ExceptionCount" shape={'circle'} />
                 </Chart>
               </Col>
             </Row>
@@ -350,7 +344,6 @@ class RegistrationForm extends React.Component {
                   <Axis name="ViewCount" visible={true} />
                   <Tooltip crosshairs={{ type: "y" }} />
                   <Geom color={['#26B99A']} type="line" position="CreateTime*ViewCount" />
-                  <Geom color={['#26B99A']} type='point' position="CreateTime*ViewCount" shape={'circle'} />
                 </Chart>
               </Col>
             </Row>
